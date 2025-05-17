32c8016c51842828f11ba0be1daf7e97a1de7762
Evgeny Tupikov <tupikov.evgeny@gmail.com>
2025-05-17 21:56:39 +0200

    Tepmporary disabled security

--- src/main/java/com/emnbc/crud_api_test/config/SecurityConfig.java
+++ src/main/java/com/emnbc/crud_api_test/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/api/users/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
